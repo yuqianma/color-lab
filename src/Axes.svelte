ce207359688d61f9cc542e8c05211a9db5b038ba
yuqianma <mayq0422@gmail.com>
2020-06-04 23:55:43 +0800

    add z axis colors

--- src/Axes.svelte
+++ src/Axes.svelte
@@ -1,6 +1,7 @@
 <script>
 import { scaleLinear } from 'd3-scale';
 import { lab } from 'd3-color';
+import { pannable } from './utils/pannable.js';
 
 const PALETTE = `#4c78a8
 #f58518
@@ -30,7 +31,7 @@ const contentOffset = borderWidth / 2;
 
 export let xDomain = [-128, 128];
 export let yDomain = [-128, 128];
-export let zDomain = [0, 100]
+export let zDomain = [0, 100];
 
 $: range = [padding, boxSize - padding];
 
@@ -38,14 +39,31 @@ $: xScale = scaleLinear(xDomain, range);
 $: yScale = scaleLinear(yDomain, range);
 
 $: zScale = scaleLinear(zDomain, range);
+
+function handlePanStart(e) {
+  console.log(e);
+}
+
+function handlePanMove(e) {
+  console.log(e);
+}
+
+function handlePanEnd(e) {
+  console.log(e);
+}
 </script>
 
 <svg width={boxSize + sideSize} height={boxSize}>
   <g transform="translate({contentOffset}, {contentOffset})">
-    <rect width={boxSize - borderWidth} height={boxSize - borderWidth} />
+    <rect class="border" width={boxSize - borderWidth} height={boxSize - borderWidth} />
     <g>
       {#each colors as color, idx}
         <circle
+          use:pannable
+          on:panstart={handlePanStart}
+          on:panmove={handlePanMove}
+          on:panend={handlePanEnd}
+          class="pannable"
           cx={xScale(getters.x(color))}
           cy={yScale(getters.y(color))}
           r=5
@@ -55,7 +73,23 @@ $: zScale = scaleLinear(zDomain, range);
     </g>
   </g>
   <g transform="translate({boxSize - contentOffset}, {contentOffset})">
-    <rect width={sideSize - borderWidth} height={boxSize - borderWidth} />
+    <rect class="border" width={sideSize - borderWidth} height={boxSize - borderWidth} />
+    <g>
+      {#each colors as color, idx}
+        <rect
+          use:pannable
+          on:panstart={handlePanStart}
+          on:panmove={handlePanMove}
+          on:panend={handlePanEnd}
+          class="pannable"
+          x={padding}
+          y={zScale(getters.z(color))}
+          width={sideSize - padding * 2}
+          height=2
+          fill={toCssColor(color)}
+        />
+      {/each}
+    </g>
   </g>
 </svg>
 
@@ -64,8 +98,12 @@ svg {
   color: #ccc;
 }
 
-rect {
+.border {
   stroke: currentColor;
   fill: none;
+}
+
+.pannable {
+  cursor: pointer;
 }
 </style>
