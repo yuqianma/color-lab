c04c285581380d2a5a5d391f2d0e5f0c04d81aa9
yuqianma <mayq0422@gmail.com>
2020-06-12 00:28:14 +0800

    add axis line

--- src/Axes.svelte
+++ src/Axes.svelte
@@ -21,7 +21,7 @@ export let boxSize = 300;
 export let sideSize = 75;
 export let padding = 10;
 
-const borderWidth = 1;
+const borderWidth = 2;
 const contentOffset = borderWidth / 2;
 
 export let xDomain = [-128, 128];
@@ -92,65 +92,106 @@ function handleMouseLeave(e) {
   hoverIdx = -1;
 }
 </script>
-<svg width={boxSize + sideSize} height={boxSize} class:modifying="{modIdx !== -1}">
-  <g transform="translate({contentOffset}, {contentOffset})">
-    <rect class="border" width={boxSize - borderWidth} height={boxSize - borderWidth} />
-    <g>
-      {#each colors as color, idx}
-        <circle
-          data-idx={idx}
-          use:pannable
-          on:panstart={handlePanStart}
-          on:panmove={handlePanMove}
-          on:panend={handlePanEnd}
-          on:mouseenter={handleMouseEnter}
-          on:mouseleave={handleMouseLeave}
-          class="color pannable"
-          class:focus="{idx === modIdx || idx === hoverIdx}"
-          cx={xScale(getters.x(color))}
-          cy={yScale(getters.y(color))}
-          fill={toCssColor(color)}
-        />
-      {/each}
-    </g>
+
+<div class="frame" style="position:absolute;--border-width: {borderWidth}px;">
+  <div class="border" style="width:{boxSize}px;height:{boxSize}px;"></div>
+  <div class="border" style="left:{boxSize}px;width:{sideSize}px;height:{boxSize}px;"></div>
+  <!-- <div class="axis-line" style="left:{borderWidth + padding}px;width:{boxSize - padding * 2}px"></div> -->
+  <!-- <div class="axis-line" style="top:{borderWidth + padding}px;width:{boxSize - padding * 2}px"></div> -->
+</div>
+
+<svg
+  width={boxSize + sideSize}
+  height={boxSize}
+  class:modifying="{modIdx !== -1}"
+  style="--border-width: {borderWidth}px"
+>
+  <g>
+    <line
+      class="axis-line"
+      x1={padding}
+      y1={boxSize / 2}
+      x2={boxSize - padding}
+      y2={boxSize / 2}
+    />
+    <line
+      class="axis-line"
+      y1={padding}
+      x1={boxSize / 2}
+      y2={boxSize - padding}
+      x2={boxSize / 2}
+    />
+    {#each colors as color, idx}
+      <circle
+        data-idx={idx}
+        use:pannable
+        on:panstart={handlePanStart}
+        on:panmove={handlePanMove}
+        on:panend={handlePanEnd}
+        on:mouseenter={handleMouseEnter}
+        on:mouseleave={handleMouseLeave}
+        class="color pannable"
+        class:focus="{idx === modIdx || idx === hoverIdx}"
+        cx={xScale(getters.x(color))}
+        cy={yScale(getters.y(color))}
+        fill={toCssColor(color)}
+      />
+    {/each}
   </g>
-  <g transform="translate({boxSize - contentOffset}, {contentOffset})">
-    <rect class="border" width={sideSize - borderWidth} height={boxSize - borderWidth} />
-    <g>
-      {#each colors as color, idx}
-        <rect
-          data-idx={idx}
-          use:pannable
-          on:panstart={handlePanStart}
-          on:panmove={handlePanMove}
-          on:panend={handlePanEnd}
-          on:mouseenter={handleMouseEnter}
-          on:mouseleave={handleMouseLeave}
-          class="color pannable"
-          class:focus="{idx === modIdx || idx === hoverIdx}"
-          style="--padding: {padding}px;--side-size: {sideSize}px;"
-          y={zScale(getters.z(color))}
-          width={sideSize - padding * 2}
-          height=1
-          stroke={toCssColor(color)}
-        />
-      {/each}
-    </g>
+  <g transform="translate({boxSize}, 0)">
+    {#each colors as color, idx}
+      <rect
+        data-idx={idx}
+        use:pannable
+        on:panstart={handlePanStart}
+        on:panmove={handlePanMove}
+        on:panend={handlePanEnd}
+        on:mouseenter={handleMouseEnter}
+        on:mouseleave={handleMouseLeave}
+        class="color pannable"
+        class:focus="{idx === modIdx || idx === hoverIdx}"
+        style="--padding: {padding}px;--side-size: {sideSize}px;"
+        y={zScale(getters.z(color))}
+        width={sideSize - padding * 2}
+        height=1
+        stroke={toCssColor(color)}
+      />
+    {/each}
   </g>
 </svg>
 
 <style>
+.frame {
+  pointer-events: none;
+}
+.frame .border {
+  position: absolute;
+  box-sizing: border-box;
+  border: var(--border-width) solid #ddd;
+}
+.frame .border:first-child {
+  border-right-width: calc(var(--border-width) / 2);
+  border-radius: 5px 0 0 5px;
+}
+.frame .border:last-child {
+  border-left-width: calc(var(--border-width) / 2);
+  border-radius: 0 5px 5px 0;
+}
+
+.frame .axis-line {
+  border: 1px solid #ddd;
+}
+
 svg {
-  color: #ccc;
 }
 
 svg.modifying {
   pointer-events: none;
 }
 
-.border {
-  stroke: currentColor;
-  fill: none;
+svg .axis-line {
+  stroke: #ddd;
+  stroke-width: var(--border-width);
 }
 
 .pannable {
